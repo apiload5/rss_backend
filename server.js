5a8b0400ffce3dce0815d5459706216b853bf0e7
apiload5 <amir03182055632@gmail.com>
2025-10-16 01:01:56 +0500

    Include database connection in server.js
    Added database connection function to server setup.

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 
 const app = require('./src/app');
 const dotenv = require('dotenv');
+const connectDB = require('./src/config/db'); // DB فنکشن شامل کریں
 
-// .env فائل سے انوائرمنٹ ویری ایبلز لوڈ کریں
 dotenv.config();
+
+// ڈیٹا بیس سے جڑیں
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
