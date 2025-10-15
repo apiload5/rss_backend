a5c25e3ab9b973a3a19d49a659b8fda3e4f307b1
apiload5 <amir03182055632@gmail.com>
2025-10-16 01:52:41 +0500

    Integrate worker service and start scheduler
    Added worker service to start scheduler on server start.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 const app = require('./src/app');
 const dotenv = require('dotenv');
-const connectDB = require('./src/config/db'); // DB فنکشن شامل کریں
+const connectDB = require('./src/config/db');
+const { startScheduler } = require('./src/services/workerService'); // Worker سروس شامل کریں
 
 dotenv.config();
 
@@ -13,5 +14,9 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    
+    // Worker Scheduler کو شروع کریں!
+    startScheduler(); 
+    
     console.log(`Environment: ${process.env.NODE_ENV}`);
 });
